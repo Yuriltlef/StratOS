a1d376e00a09e22ce4e2b7b7c022a6ff9f794029
yurilt_s <33693@qq.com>
2026-03-28 18:08:54 +0800

    初步完成上下文切换策略处理器

---
+++ os_hal/include/system_control.hpp
@@ -0,0 +1,16 @@
+/**
+ * @file system_control.hpp
+ * @author your name (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2026-03-28
+ * 
+ * @copyright Copyright (c) 2026
+ * 
+ */
+#pragma once
+
+#ifndef STRATOS_HAL_SYSTEM_CONTROL_HPP
+#define STRATOS_HAL_SYSTEM_CONTROL_HPP
+
+#endif
