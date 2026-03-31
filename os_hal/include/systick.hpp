3b5d3ef76a9d7a435ab5e5cb2d29d680b3b96c39
YT_Minro <yurilt15312@outlook.com>
2026-03-31 14:50:50 +0800

    修复了中断控制器priority_type作为方法set_priority参数时的类型错误问题，确保其与策略类定义的一致。同时添加了相关的编译期断言来验证策略类是否正确地定义了priority_type及其有效性。这些更改增强了代码的类型安全性和可维护性。

---
+++ os_hal/include/systick.hpp
@@ -0,0 +1,26 @@
+/**
+ * @file systick.hpp
+ * @author your name (you@domain.com)
+ * @brief
+ * @version 0.1
+ * @date 2026-03-31
+ *
+ * @copyright Copyright (c) 2026
+ *
+ */
+#pragma once
+
+#ifndef STRATOS_HAL_SYSTICK_HPP
+#define STRATOS_HAL_SYSTICK_HPP
+
+namespace strat_os::hal::traits
+{
+
+} // namespace strat_os::hal::traits
+
+namespace strat_os::hal
+{
+
+} // namespace strat_os::hal
+
+#endif // STRATOS_HAL_SYSTICK_HPP
