84138d6624cb9517347b399ea026a250724ee719
YT_Minro <yurilt15312@outlook.com>
2026-04-10 00:48:31 +0800

    优化内核结构组织，按模块拆分进单独文件夹

---
+++ os_kernel/include/core/task/task.hpp
@@ -0,0 +1,20 @@
+/**
+ * @file task.hpp
+ * @author your name (you@domain.com)
+ * @brief
+ * @version 0.1
+ * @date 2026-04-09
+ *
+ * @copyright Copyright (c) 2026
+ *
+ */
+#pragma once
+
+#ifndef STRATOS_KERNEL_TASK_HPP
+#define STRATOS_KERNEL_TASK_HPP
+
+namespace strat_os::kernel::traits {
+
+}
+
+#endif // STRATOS_KERNEL_TASK_HPP
